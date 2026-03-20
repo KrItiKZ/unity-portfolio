9d5f52c2feb84b7c7893ae1a0311dbc00b62f5de
KrItiKZ <98580571+KrItiKZ@users.noreply.github.com>
2026-03-20 21:28:17 +0300

    Delete Assets/procedure_scripts directory

--- Assets/procedure_scripts/FakeRoom/FakeRoomConfig.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public class FakeRoomConfig : MonoBehaviour
-{
-    public Vector3 realRoomOffset = Vector3.zero;
-    public Quaternion realRoomRotation = Quaternion.identity;
-}
--- Assets/procedure_scripts/FakeRoom/Ladder/Ladder.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public class Ladder : MonoBehaviour
-{
-    public Vector3 direction = Vector3.up;
-}
